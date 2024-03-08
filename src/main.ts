8be84a0ee30c1d2fcf80eb337363e007cd2f474b
Pavel <verbipo@gmail.com>
2024-03-08 13:12:11 +0100

    feat: configure swager and env port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
+import { load } from 'js-yaml';
+import { readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('PORT', 4001);
+
+  const file = await readFile(join(__dirname, '..', 'doc', 'api.yaml'), 'utf8');
+  const document = load(file) as OpenAPIObject;
+  SwaggerModule.setup('doc', app, document);
+
+  await app.listen(PORT, () => {
+    console.log(`Server is running: http://localhost:${PORT}`),
+      console.log(`OpenAPI documentation: http://localhost:${PORT}/doc`);
+  });
 }
+
 bootstrap();
