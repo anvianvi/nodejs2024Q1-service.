bdcb845d090b75a56c31e7e6249fddc0fb0be849
Pavel <verbipo@gmail.com>
2024-03-10 21:04:12 +0100

    feat: add and remove favorites

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
 
 @Controller('favs')
@@ -9,5 +17,41 @@ export class FavoritesController {
   @HttpCode(HttpStatus.OK)
   getAllAlbums() {
     return this.favoritesService.getAllFavorites();
+  }
+
+  @Post('track/:id')
+  @HttpCode(HttpStatus.CREATED)
+  addTrack(@Param('id') id: string) {
+    return this.favoritesService.addFavorite(id, 'track');
+  }
+
+  @Delete('track/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  deleteTrack(@Param('id') id: string) {
+    return this.favoritesService.deleteFavorite(id, 'track');
+  }
+
+  @Post('album/:id')
+  @HttpCode(HttpStatus.CREATED)
+  addAlbum(@Param('id') id: string) {
+    return this.favoritesService.addFavorite(id, 'album');
+  }
+
+  @Delete('album/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  deleteAlbum(@Param('id') id: string) {
+    return this.favoritesService.deleteFavorite(id, 'album');
+  }
+
+  @Post('artist/:id')
+  @HttpCode(HttpStatus.CREATED)
+  addArtist(@Param('id') id: string) {
+    return this.favoritesService.addFavorite(id, 'artist');
+  }
+
+  @Delete('artist/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  deleteArtist(@Param('id') id: string) {
+    return this.favoritesService.deleteFavorite(id, 'artist');
   }
 }
--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -1,9 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { database } from 'src/database/storage';
+import { Album, Artist, Track } from 'src/types';
+import { validate } from 'uuid';
 
 @Injectable()
 export class FavoritesService {
   getAllFavorites() {
     return database.favorites;
+  }
+
+  addFavorite(id: string, entity: string) {
+    if (!validate(id)) {
+      throw new HttpException(
+        `Bad request. ${entity}Id is invalid (not uuid)`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    const elementIndex = database[`${entity}s`].findIndex(
+      (elem: Artist | Track | Album) => elem.id === id,
+    );
+
+    if (elementIndex === -1) {
+      throw new HttpException(
+        `${entity} was not found.`,
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    database.favorites[`${entity}s`].push(id);
+  }
+
+  deleteFavorite(id: string, entity: string) {
+    if (!validate(id)) {
+      throw new HttpException(
+        `Bad request. ${entity}Id is invalid (not uuid)`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const elementIndex = database.favorites[`${entity}s`].findIndex(
+      (elem: string) => elem === id,
+    );
+
+    if (elementIndex === -1) {
+      throw new HttpException(`${entity} was not found.`, HttpStatus.NOT_FOUND);
+    }
+
+    database.favorites[`${entity}s`] = database.favorites[`${entity}s`].filter(
+      (elem: string) => elem !== id,
+    );
   }
 }
